c6950cd91194545b4086ce14054bd7bb709501c7
tcaish <tim.caish@foolwealth.com>
2022-10-08 14:31:26 -0400

    added shrink animation to egg; hatch sound plays when egg starts shrinking

--- src/components/egg/egg.jsx
+++ src/components/egg/egg.jsx
@@ -1,12 +1,27 @@
 import { useEffect } from 'react';
+import useSound from 'use-sound';
 import { animateCSS } from '../../exports/functions';
+import HatchSound from '../../assets/sounds/hatch.mp3';
 import './egg.scss';
 
 function Egg({ id, style }) {
+  const [playHatchSound] = useSound(HatchSound);
+
   // Animate the egg
   useEffect(() => {
-    animateCSS(`#${id}`, 'egg-wobble', false);
-  }, [id]);
+    const eggId = `#${id}`;
+    const node = document.querySelector(eggId);
+
+    // Wobble
+    animateCSS(eggId, 'egg-wobble', false).then(() => {
+      playHatchSound();
+
+      // Shrink
+      animateCSS(eggId, 'shrink', false).then(() => {
+        node.parentElement.removeChild(node);
+      });
+    });
+  }, [id, playHatchSound]);
 
   return <div id={id} className="egg" style={style}></div>;
 }
