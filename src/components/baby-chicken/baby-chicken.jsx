2623cf8390e0f34fb621f8a499d967258e0ed7ee
tcaish <tim.caish@foolwealth.com>
2022-10-09 15:27:21 -0400

    baby chickens now are turned the correct way based on what side of screen they are on

--- src/components/baby-chicken/baby-chicken.jsx
+++ src/components/baby-chicken/baby-chicken.jsx
@@ -16,17 +16,12 @@ function BabyChicken(props) {
 
     // Grow and move off screen
     let moveDirection = '100vw'; // Move to the right off-screen
-    let lookDirection = '1'; // Baby chicken looks to right side
+    let lookDirection = '180deg'; // Baby chicken looks to right side
     const isLeftSide = isBabyChickenOnLeftSide(props.style.left);
-
-    if (isLeftSide) {
-      moveDirection = '-100vw';
-    } else {
-      lookDirection = '-1';
-    }
+    isLeftSide && (moveDirection = '-100vw') && (lookDirection = '0deg');
 
     babyChicken.style.setProperty('--move-off-screen-direction', moveDirection);
-    // babyChicken.style.setProperty('--baby-chicken-flip', lookDirection);
+    babyChicken.style.setProperty('--baby-chicken-flip', lookDirection);
     babyChicken.classList.add('grow-and-move-off-screen');
 
     // Change baby chicken image repeatedly
