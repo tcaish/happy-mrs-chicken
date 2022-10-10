2bea8533b16e0bf0911e907d56e90074384e5a83
tcaish <tim.caish@foolwealth.com>
2022-10-10 10:22:20 -0400

    removed unused classes

--- src/classes/BabyChicken.js
+++
@@ -1,7 +0,0 @@
-export class BabyChicken {
-  constructor(id, key, style) {
-    this.id = id;
-    this.key = key;
-    this.style = style;
-  }
-}
--- src/classes/Egg.js
+++
@@ -1,7 +0,0 @@
-export class Egg {
-  constructor(id, key, style) {
-    this.id = id;
-    this.key = key;
-    this.style = style;
-  }
-}
