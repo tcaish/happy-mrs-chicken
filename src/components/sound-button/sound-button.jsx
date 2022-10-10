06a3e424a4ca576610235fb88a8a2b6cda51bc61
tcaish <tim.caish@foolwealth.com>
2022-10-10 13:18:30 -0400

    sound button mobile friendly

--- src/components/sound-button/sound-button.jsx
+++ src/components/sound-button/sound-button.jsx
@@ -1,5 +1,6 @@
 import { BsFillVolumeUpFill, BsFillVolumeMuteFill } from 'react-icons/bs';
 import './sound-button.scss';
+import './sound-button.mobile.scss';
 
 function SoundButton({ soundOn, setSoundOn }) {
   return (
