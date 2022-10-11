9bbaa869b426535e455b927f4a89595b001cc92d
tcaish <tim.caish@foolwealth.com>
2022-10-11 10:09:14 -0400

    updated chicken size on mobile

--- src/components/chicken/chicken.jsx
+++ src/components/chicken/chicken.jsx
@@ -1,21 +1,32 @@
 import { useEffect } from 'react';
 import './chicken.scss';
+import './chicken.mobile.scss';
 
 function Chicken({ firstLoad, chickenImage }) {
   // On first load, set chicken to middle of screen
   useEffect(() => {
     if (firstLoad) {
       const chicken = document.querySelector('.chicken');
+      const screenWidth = window.innerWidth;
+
+      let chickenHeight = 145.88;
+      let chickenWidth = 150;
+
+      // If on mobile device
+      if (screenWidth <= 576) {
+        chickenHeight = 117.3;
+        chickenWidth = 120;
+      }
 
       // Divide by 2 to get top-left corner to middle of screen; subtract
       // width of chicken / 2 to get it in the middle
       chicken.style.setProperty(
         '--chicken-vertical',
-        `${window.innerHeight / 2 - 145.88 / 2 - 40}px`
+        `${window.innerHeight / 2 - chickenHeight / 2 - 40}px`
       );
       chicken.style.setProperty(
         '--chicken-horizontal',
-        `${window.innerWidth / 2 - 150 / 2}px`
+        `${window.innerWidth / 2 - chickenWidth / 2}px`
       );
     }
   });
